761cd3b545db7f0c4b44cf8a09b2426296f8a7c5
RomanS-re <roman@orion.money>
2022-07-05 15:30:18 +0400

    added nlb sg id output

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "nlb_endpoint" {
   description = "NLB endpoint to accsess Chainlink Node UI. UI port is open only to the VPC CIDR block, in order to access the UI it's required to open SSH tunnel to any available host/bastion in the VPC. More info in Readme.md"
   value       = "${aws_lb.this.dns_name}:6688"
 }
+
+output "nlb_security_group_id" {
+  description = "ID of security group attached to NLB. It's possible to use it to configure additional sg inbound rules"
+  value       = aws_security_group.this.id
+}
