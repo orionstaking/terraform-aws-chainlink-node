e6c09510f5358fa75ff9713a5e401fdec0733dd5
Roman S <roman@orion.money>
2022-11-09 17:19:52 +0400

    fix: enable preserve_client_ip on target_groups to restrict ui access

--- alb.tf
+++ alb.tf
@@ -23,6 +23,7 @@ resource "aws_lb_target_group" "ui" {
   target_type          = "ip"
   vpc_id               = var.vpc_id
   deregistration_delay = 10
+  preserve_client_ip   = true
 
   health_check {
     enabled             = true
@@ -42,6 +43,7 @@ resource "aws_lb_target_group" "node" {
   target_type          = "ip"
   vpc_id               = var.vpc_id
   deregistration_delay = 10
+  preserve_client_ip   = true
 
   health_check {
     enabled             = true
