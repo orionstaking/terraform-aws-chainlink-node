6d9a032867c860768dc56515bc8c32348f1fa3f4
Roman S <roman@orion.money>
2023-02-28 19:17:23 +0400

    fix parse_config

--- parse_config.py
+++ parse_config.py
@@ -95,18 +95,24 @@ def parse_config(config):
     except KeyError:
         pass
 
-    if p2p_v2 and not p2p_v1:
+    if p2p_v1 and not p2p_v2:
         p2p_networking = "V1"
         try:
             announce_ip = verify_announce_ip(config['P2P']['V1']['AnnounceIP'], tf_announce_ips)
             announce_port = config['P2P']['V1']['AnnouncePort']
             listen_ip = verify_listen_ip(config['P2P']['V1']['ListenIP'])
             listen_port = config['P2P']['V1']['ListenPort']
+            announce_addresses = []
+            listen_addresses = []
         except KeyError:
             key_err_msg("P2P.V1.AnnounceIP, P2P.V1.AnnouncePort, P2P.V1.ListenIP, P2P.V1.ListenPort")
-    elif p2p_v1 and not p2p_v2:
+    elif p2p_v2 and not p2p_v1:
         p2p_networking = "V2"
         try:
+            announce_ip = []
+            announce_port = []
+            listen_ip = []
+            listen_port = []
             announce_addresses = verify_announce_addresses(config['P2P']['V2']['AnnounceAddresses'], tf_announce_ips)
             listen_addresses = verify_listen_addresses(config['P2P']['V2']['ListenAddresses'])
         except KeyError:
